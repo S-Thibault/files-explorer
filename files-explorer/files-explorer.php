1c97ec325edf2d45e1a64cffc85f025d09e53fa6
S-Thibault <sylvain.thibault@infamail.com>
2020-06-02 16:39:48 +0200

    Add files via upload

---
+++ files-explorer/files-explorer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>files-explorer</title>
+  </head>
+  <body>
+
+    <?php
+
+  echo "sylvain";
+
+  ?>
+  </body>
+</html>
